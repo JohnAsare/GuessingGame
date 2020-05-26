cfc356f1f5f45df030aa03832f862eff46939647
JohnAsare <johnasare7@gmail.com>
2020-05-26 19:59:45 -0400

    Tested the function and it works great

--- GuessingGame.py
+++ GuessingGame.py
@@ -7,18 +7,18 @@ from random import randint
 
 
 def guess_the_number(user_number):
-    intro = 'If you guess the correct number on my card, you will win $100. If you guessed wrong,' \
-            'I will take $50 from you.'
+
     winning_anoucement = 'You have won $100!'
     lossing_anoucement= 'You lost, give me $50 :('
 
-    print(intro)
     my_card_number = randint(0, 10000000)
 
     if user_number == my_card_number:
         print(f'{winning_anoucement}')
     else:
-        print(f'{lossing_anoucement}')
+        print(f'{lossing_anoucement}, the correct number is {my_card_number}')
 
 
+print('If you guess the correct number on my card, you will win $100. If you guessed wrong,'
+          'I will take $50 from you.')
 guess_the_number(int(input('\n What is the number on my card?: ')))
