8bb65a2f0c441600a60802cf41a8e29c62ad5b3d
JohnAsare <johnasare7@gmail.com>
2020-05-26 19:51:15 -0400

    Adding the function code

--- GuessingGame.py
+++ GuessingGame.py
@@ -3,5 +3,18 @@
 
 
 """ This is going to be a game of guessing numbers"""
+from random import randint
 
-def
+
+def guess_the_number(user_number):
+    intro = 'If you guess the correct number on my card, you will win $100. If you guessed wrong,' \
+            'I will take $50 from you.'
+    winning_anoucement = 'You have won $100!'
+    lossing_anoucement= 'You lost, give me $50 :('
+
+    print(intro)
+    user_number = int(input('\n What is the number on my card?:'))
+    my_card_number = randint(0, 10000000)
+
+    if user_number == my_card_number:
+        print('f'{winning_anoucement})
